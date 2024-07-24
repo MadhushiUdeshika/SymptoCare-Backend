2321c195bc5098e9f8670af0e52460179ae0a3d5
Madhushi Udeshika <pkmudeshika@gmail.com>
2024-07-24 19:06:01 +0530

    Update application and controller files

---
+++ user-service/src/main/java/org/springframework/security/config/annotation/web/configuration/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,18 @@
+package org.springframework.security.config.annotation.web.configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class WebSecurityConfigurerAdapter {
+
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'authenticationManagerBean'");
+    }
+
+}
